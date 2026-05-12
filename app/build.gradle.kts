a95c9de20afe6661264b6cbcae5a109748cc91f6
FlashFare Dev <contact@apesoft.fr>
2026-05-12 19:07:59 +0200

    chore: enable viewbinding and add accessibility service description

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -30,8 +34,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
